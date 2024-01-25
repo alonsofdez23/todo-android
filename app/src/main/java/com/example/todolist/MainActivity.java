0866a7ad252d9017ad8695a421fb02e891d6d743
alonsofdez23 <alonsobs23@gmail.com>
2024-01-25 22:25:30 +0100

    add create task

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -9,13 +9,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private String idUser;
+
+    private ListView listViewTareas;
+    private List<String> listaTareas = new ArrayList<>();
+    private List<String> listaIDTareas = new ArrayList<>();
+    private ArrayAdapter<String> mAdapterTareas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Inicializar Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance();
+        idUser = mAuth.getCurrentUser().getUid();
+
+        listViewTareas = findViewById(R.id.listTareas);
     }
 
     @Override
@@ -46,7 +69,27 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             // Añadir tarea a la base de datos y al listView
-                            Toast.makeText(MainActivity.this, "Tarea añadida", Toast.LENGTH_SHORT). show();
+                            String tarea = taskEditText.getText().toString();
+
+                            Map<String, Object> miTarea = new HashMap<>();
+                            miTarea.put("nombreTarea", tarea);
+                            miTarea.put("idUsuario", idUser);
+
+                            // Add a new document with a generated ID
+                            db.collection("Tareas")
+                                .add(miTarea)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Toast.makeText(MainActivity.this, "Tarea añadida", Toast.LENGTH_SHORT). show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(MainActivity.this, "Fallo al crear la tarea", Toast.LENGTH_SHORT). show();
+                                    }
+                                });
                         }
                     })
                     .setNegativeButton("Cancelar", null)
